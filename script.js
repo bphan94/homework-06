76f9df53ad40010ab9f4113fe541186ad2ff0c75
Benjamin Phan <benjaminphan94@gmail.com>
2020-10-24 19:49:05 -0400

    working on script

---
+++ script.js
@@ -0,0 +1,9 @@
+var todaysDate = moment().format("M/D/YYY");
+var key = //put api key 
+var city = "";
+var latitude = "";
+var longitude = "";
+var weather = "";
+var icon = "";
+var cityArr = "";
+
